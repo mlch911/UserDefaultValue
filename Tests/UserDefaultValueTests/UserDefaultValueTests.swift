eaeeee7709eb315830f2b19303886fb8efe74770
mlch911 <mlch911@mluoc.tech>
2022-08-08 16:00:25 +0800

    Add Test

--- Tests/UserDefaultValueTests/UserDefaultValueTests.swift
+++ Tests/UserDefaultValueTests/UserDefaultValueTests.swift
@@ -2,10 +2,30 @@ import XCTest
 @testable import UserDefaultValue
 
 final class UserDefaultValueTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(UserDefaultValue().text, "Hello, World!")
+    func test() throws {
+		UserDefaults.resetStandardUserDefaults()
+		
+		XCTAssertEqual(UserDefaults.testString, "")
+		XCTAssertEqual(UserDefaults.testInt, 0)
+		XCTAssertEqual(UserDefaults.testDouble, 0.0)
+		
+		UserDefaults.testString = "Hello, World!"
+		UserDefaults.testInt = 100
+		UserDefaults.testDouble = 99.99
+		
+		XCTAssertEqual(UserDefaults.testString, "Hello, World!")
+		XCTAssertEqual(UserDefaults.testInt, 100)
+		XCTAssertEqual(UserDefaults.testDouble, 99.99)
     }
+}
+
+extension UserDefaults {
+	@UserDefaultValue(key: "testString")
+	static var testString = ""
+	
+	@UserDefaultValue(key: "testInt")
+	static var testInt = 0
+	
+	@UserDefaultValue(key: "testDouble")
+	static var testDouble = 0.0
 }
